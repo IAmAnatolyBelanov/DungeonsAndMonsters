abf877a0a2a282d0856574db25fe67755c610f07
anatoly.belanov.0798@gmail.com <anatoly.belanov.0798@gmail.com>
2022-01-25 20:34:16 +0300

    Added validation to layers

--- Assets/Scripts/Layer/LayerId.cs
+++ Assets/Scripts/Layer/LayerId.cs
@@ -1,13 +1,31 @@
+using System;
 using UnityEngine;
 
 public static class LayerId
 {
     public static class Character
     {
-        public static int BackId = SortingLayer.NameToID(Constant.Layer.CharactersBack);
+        public static readonly int BackId;
 
-        public static int MiddleId = SortingLayer.NameToID(Constant.Layer.CharactersMiddle);
+        public static readonly int MiddleId;
 
-        public static int FrontId = SortingLayer.NameToID(Constant.Layer.CharactersFront);
+        public static readonly int FrontId;
+
+        static Character()
+        {
+            BackId = GetLayersId(Constant.Layer.CharactersBack);
+            MiddleId = GetLayersId(Constant.Layer.CharactersMiddle);
+            FrontId = GetLayersId(Constant.Layer.CharactersFront);
+        }
+    }
+
+    private static int GetLayersId(string layerName)
+    {
+        var id = SortingLayer.NameToID(layerName);
+
+        if (id == 0)
+            throw new ArgumentException($"Can't find layer with name {layerName}", nameof(layerName));
+
+        return id;
     }
 }
